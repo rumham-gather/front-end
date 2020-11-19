53ea1254726fbd802cc8e6f185278f72332cfada
Your NamejeffIrvine <jirvine1212@gmail.com>
2020-11-19 14:59:46 -0800

    added photo for bio

--- src/Components/RecipesList.js
+++ src/Components/RecipesList.js
@@ -20,7 +20,19 @@ export default class RecipeListPage extends Component {
     render() {
         return (
             <div>
-                Look at all those recipes
+                <div className='drop-downs'>
+                    <select onChange={this.props.handleSortOrder}>
+                        <option value=''>Order</option>
+                        <option value='asc'>Ascending Order</option>
+                        <option value='desc'>Descending Order</option>
+                    </select>
+                    <select onChange={this.props.handleSortType}>
+                        <option value='' >Sort By</option>
+                        <option value='attack' >Attack</option>
+                        <option value='defense'>Defense</option>
+                        <option value='type'>Type</option>
+                    </select>
+                </div>
                 {
                     this.state.recipes.length === 0
                     ? <div>Simmering...</div>
