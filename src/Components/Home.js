89ea9df4e2c7df6ebd369ae144bff82e02d172b5
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-11-19 23:04:07 -0800

    home page setup and css

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
+import gatherVideo from './Video/gather.mp4';
 import ScrollMenu from '../Burger/Scrollmenu.js';
+import '../Burger/scroll.css';
 
 export default class Home extends Component {
     render() {
         return (
+           <div>
             <main>
+                
                 <div className="homepage">
-                    HOME PAGE
-                    <ScrollMenu />
-                    
-                    
-
+                    <div className="video">
+                    <video autoPlay loop muted
+                    style={{
+                        position: "absolute",
+                        width: "100%",
+                        left: "50%",
+                        top: "25%",
+                        height: "90%",
+                        objectFit: "cover",
+                        transform: "translate(-50%, -50%)",
+                        zIndex: "-1",
+                    }}>
+                    <source src={gatherVideo} type="video/mp4"/>
+                    </video>
+                    </div>
+                    <div className="content">
+                        <h1>Home for the Holidays</h1>
+                        <h3>Gather is the ultimate collaborative food resource for the home cook, with daily kitchen tips, and, oh yeah, over 5000+recipes all at your fingertips.</h3>
+                    </div>
+                    <div className="scroll-bar">
+                        <h1>Our Newest Recipes</h1>
+                            <ScrollMenu/>
+                    </div>
                 </div>
             </main>
-            
+        </div> 
         )
     }
 }
