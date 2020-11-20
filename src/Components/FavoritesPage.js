d0aecb3a0ad4a9e9236497b2f78f66dde176d8af
Jena Boehm <boehmjena@gmail.com>
2020-11-19 17:45:20 -0800

    functional delete favorites button

--- src/Components/FavoritesPage.js
+++ src/Components/FavoritesPage.js
@@ -31,6 +31,14 @@ export default class FavoritesPage extends Component {
                 recipes: recipes.map(item => item.body) });
     }
 
+    handleDelete = async (id) => {
+        await request 
+            .delete(`${process.env.REACT_APP_BACK_END_URL}/api/user-recipes/${id}`)
+            .set('Authorization', this.props.token)
+
+        await this.fetchFavorites();
+    }
+
     render() {
         console.log(this.state.favorites);
         return (
@@ -38,6 +46,8 @@ export default class FavoritesPage extends Component {
                 {
                     !!this.state.recipes.length 
                     ? this.state.recipes.map(recipe => {
+                        const favorite = this.state.favorites.find(favorite => favorite.recipe_id === recipe.id)
+
                         return (
                         <div>
                             <Link to={`/recipes/${recipe.id}`}>
@@ -46,6 +56,7 @@ export default class FavoritesPage extends Component {
                                 <p className='title'>{recipe.title}</p>
                             </div>
                             </Link>
+                            <button onClick={() => this.handleDelete(favorite.id)}>Delete from Recipe Box</button>
                         </div>
                         )
                     })
