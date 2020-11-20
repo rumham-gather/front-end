e78f5d31f3daba56c8c1d84ddbeee029da6e0606
Your NamejeffIrvine <jirvine1212@gmail.com>
2020-11-19 17:23:53 -0800

    deleted dropdown

--- src/Components/RecipesList.js
+++ src/Components/RecipesList.js
@@ -15,24 +15,12 @@ export default class RecipeListPage extends Component {
         const response = await request.get(`${process.env.REACT_APP_BACK_END_URL}/api/recipes`)
         .set('Authorization', this.props.token)
         this.setState({ recipes: response.body.recipes })
+        console.log(response);
     }
 
     render() {
         return (
             <div>
-                <div className='drop-downs'>
-                    <select onChange={this.props.handleSortOrder}>
-                        <option value=''>Order</option>
-                        <option value='asc'>Ascending Order</option>
-                        <option value='desc'>Descending Order</option>
-                    </select>
-                    <select onChange={this.props.handleSortType}>
-                        <option value='' >Sort By</option>
-                        <option value='attack' >Attack</option>
-                        <option value='defense'>Defense</option>
-                        <option value='type'>Type</option>
-                    </select>
-                </div>
                 {
                     this.state.recipes.length === 0
                     ? <div>Simmering...</div>
