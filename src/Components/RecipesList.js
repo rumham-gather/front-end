c4c1a17c84278ba44029553395aaa91f3fadc6ff
Your NamejeffIrvine <jirvine1212@gmail.com>
2020-11-19 20:32:17 -0800

    working reset button

--- src/Components/RecipesList.js
+++ src/Components/RecipesList.js
@@ -18,9 +18,19 @@ export default class RecipeListPage extends Component {
         console.log(response);
     }
 
+    handleClick = async () => {
+        const response = await request.get(`${process.env.REACT_APP_BACK_END_URL}/api/recipes`)
+        .set('Authorization', this.props.token)
+        this.setState({ recipes: response.body.recipes })
+        console.log(response);
+    }
+
     render() {
         return (
             <div>
+                <div className='reset-button'>
+                    <button onClick={() => this.handleClick()}>More food!</button>
+                </div>
                 {
                     this.state.recipes.length === 0
                     ? <div>Simmering...</div>
